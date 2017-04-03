f76f5f639c50907e1d042f8f32df0b2d3d189bea
github username <github email address>
2017-04-03 11:46:53 -0400

    learn about yielding

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,21 @@
 require 'pry'
 
 def my_all?(collection)
+	i = 0
+	block_return_values = []
+	while i < collection.length
+		block_return_values << yield(collection[i])
+		i += 1
+	end
+	puts block_return_values
 
+	if block_return_values.include?(false)
+		false
+	else
+		true
+	end
+end
+
+my_all?([1, 2, 3]) do |i|
+	i < 2
 end
